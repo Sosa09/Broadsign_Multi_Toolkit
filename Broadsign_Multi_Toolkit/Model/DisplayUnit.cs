b84645ee123327e9ebc1d29fcf72c6c04fec6abb
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-10-21 22:44:36 +0200

    Initializingn all base classes and folders

---
+++ Broadsign_Multi_Toolkit/Model/DisplayUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broadsign_Multi_Toolkit.Model
+{
+    internal class DisplayUnit
+    {
+    }
+}
