f82c4f76114559c9223f89d58dc3dd25a38256e3
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-10-21 22:57:58 +0200

    implemeneted properties for day part model

--- Broadsign_Multi_Toolkit/Model/DayPart.cs
+++ Broadsign_Multi_Toolkit/Model/DayPart.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Broadsign_Multi_Toolkit.Model
 {
-    internal class DayPart
+    public class DayPart : IBroadsignModel
     {
+        public string Active { get; set; } 
+        public string DayMask { get; set; }
+        public string DomainId { get; set; }
+        public string EndDate { get; set;}
+        public string EndTime { get; set;}
+        public string Id { get; set;}
+        public string ImpressionsPerHour { get; set;}
+        public string MinuteMask { get; set;}
+        public string Name { get; set;}
+        public string ParentId { get; set;}
+        public string StartDate { get; set;}
+        public string StartTime { get; set;}
+        public string VirtualEndDate { get; set;}
+        public string VirtualStartDate { get; set;}
+        public string Wheight { get; set;}
+
     }
 }
---
+++ Broadsign_Multi_Toolkit/Model/IBroadsignModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Broadsign_Multi_Toolkit.Model
+{
+    public interface IBroadsignModel
+    {
+        string Name { get; }
+        public string Id { get; set; }
+    }
+}
