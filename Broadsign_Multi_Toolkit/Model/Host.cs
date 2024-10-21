4d13a82e0b0adf06997c0cc7d7e2645cc929765b
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-10-21 23:16:04 +0200

    Host implemented properties model

--- Broadsign_Multi_Toolkit/Model/Host.cs
+++ Broadsign_Multi_Toolkit/Model/Host.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Broadsign_Multi_Toolkit.Model
 {
-    internal class Host
+    internal class Host : IBroadsignModel
     {
+        public string Name { get ; set ; }
+        public string Id { get ; set ; }
+        public string Active { get ; set ; }
+        public string ConfigProfileBagId { get ; set ; }
+        public string ContainerId { get ; set ; }
+        public string CustomUniqueId { get ; set ; }
+        public string DiscoveryStatus { get ; set ; }
+        public string DisplayUnitId { get ; set ; }
+        public string DomainId { get ; set ; }
+        public string Geolocation { get ; set ; }
+        public string Nscreens { get ; set ; }
+        public string PrimaryMacAddress { get ; set ; }
+        public string PublicKeyFingerprint { get ; set ; }
+        public string RemoteClearDbTmUtc { get ; set ; }
+        public string RemoteRebootTmUtc { get ; set ; }
+        public string SecondaryMacAddress { get ; set ; }
+        public string Volume { get ; set ; }
+
     }
 }
