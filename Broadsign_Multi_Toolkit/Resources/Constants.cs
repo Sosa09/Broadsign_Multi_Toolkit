9a124da36d38b9a8a9337f363980c1f62a1da86d
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-10-21 22:52:17 +0200

    set up all base paths reqquired to make api calls as constants Display unit skin daypart host and the endpoint

--- Broadsign_Multi_Toolkit/Resources/Constants.cs
+++ Broadsign_Multi_Toolkit/Resources/Constants.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Broadsign_Multi_Toolkit.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace Broadsign_Multi_Toolkit.Resources
 {
     internal class Constants
     {
+        const string ENDPOINT = @"https://api.broadsign.com:10899/rest";
+        const string DISPLAY_UNIT_GET_PATH = @"/display_unit/v12";
+        const string DISPLAY_UNIT_POST_PATH = @"/display_unit/v12/add";
+        const string DISPLAY_UNIT_PUT_PATH = @"/display_unit/v12"; 
+        const string SKIN_GET_PATH = @"/skin/v7";
+        const string SKIN_UNIT_POST_PATH = @"/skin/v7add";
+        const string SKIN_UNIT_PUT_PATH = @"/skin/v7"; 
+        const string HOST_UNIT_GET_PATH = @"/host/v17";
+        const string HOST_UNIT_POST_PATH = @"/host/v17""add";
+        const string HOST_UNIT_PUT_PATH = @"/host/v17"; 
+        const string DAYPART_UNIT_GET_PATH = @"/day_part/v5";
+        const string DAYPART_UNIT_POST_PATH = @"/day_part/v5/add";
+        const string DAYPART_UNIT_PUT_PATH = @"/day_part/v5/";
+
     }
 }
